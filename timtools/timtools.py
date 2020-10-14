a164ba183a9d7e7ebb798d783fe8e94eb7b6c7db
Tim Moore <tcmoore3@gmail.com>
2020-10-14 09:41:12 -0400

    Add functions for getting configurations

--- timtools/timtools.py
+++ timtools/timtools.py
@@ -4,31 +4,78 @@ Some functions I find myself rewriting frequently
 
 Handles the primary functions
 """
+import freud
+import gsd.hoomd
 
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
+TTDEFAULTS = {
+        'is2D': True,
+        }
 
-    Replace this function and doc string for your own project
+
+def _get_configuration(frame, is2D=TTDEFAULTS['is2D']):
+    # get box and positions and orientations from frame
+    box = frame.configuration.box
+    if is2D:
+        fbox = freud.box.Box(Lx=box[0], Ly=box[1], xy=box[3], is2D=True)
+    else:
+        fbox = freud.box.Box(Lx=box[0], Ly=box[1], Lz=box[2],
+                xy=box[3], xz=box[4], yz=box[5])
+    pos = frame.particles.position
+    orientation = frame.particles.orientation
+    return fbox, pos, orientation
+
+def get_gsd_configurations(
+        filename,
+        start_frame=None,
+        end_frame=None,
+        frame_skip=None,
+        is2D=TTDEFAULTS['is2D']):
+    """Get box, positions, and orientations of particles in trajectory
 
     Parameters
     ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
+    filename : str
+        The filename of the trajectory to open
+    start_frame : int or None
+        The frame to start on
+    end_frame : int or None
+        The frame to end on
+    frame_skip : int or None
+        The stride length
+    is2D : bool, default=True
+        Whether or not to return a 2D box
+
+    Yields
+    ------
+    box : freud.box.Box
+        The simulation box
+    positions : np.ndarray, shape=(n, 3)
+        The particle positions
+    orientations : np.ndarray, shape=(n, 4)
+        The particle orientations
+
+    """
+    traj_slice = slice(start_frame, end_frame, frame_skip)
+    with gsd.hoomd.open(filename, 'rb') as traj:
+        for frame in traj[traj_slice]:
+            yield _get_configuration(frame)
+
+def get_N_particles(filename, frame=0):
+    """Get the number of particles in a specific frame of a trajectory
+
+    Parameters
+    ----------
+    filename : str
+        The filename
+    frame : int, default=0
 
     Returns
     -------
-    quote : str
-        Compiled string including quote and optional attribution
+    N : int
+        The number of particles in the specified frame
+
     """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
-
-
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+    with gsd.open(filename, 'rb') as traj:
+        N = traj[frame].particles.N
+    return N
